7930a18b09d251bee8eabdb8a7c0de7472245cb2
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-06-09 20:50:23 +0600

    num

---
+++ 4_number.php
@@ -0,0 +1,31 @@
+<?php
+// oder >  {B I D M A S }
+$radius = 25;
+$pi = 3.14;
+//echo floor ($pi); //output 3
+//echo ceil ($pi); //output 4
+
+
+$age = 20;
+//$age += 10;echo $age;  //30
+//$age -= 10;echo $age;  //10
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>vnum</title>
+</head>
+
+<body>
+</body>
+
+</html>
