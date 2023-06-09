8f914600c4c21a9b55d871f1ce0097980818c0d4
Mahbub Hasaan Riyad <riyad.info1@gmail.com>
2023-06-09 20:28:21 +0600

    String

---
+++ 3_string.php
@@ -0,0 +1,42 @@
+<?php
+
+$stringOne = " my email address";
+$stringTwo = "riyad.info1@gmail.com";
+//echo $stringOne . $stringTwo;
+
+$name = 'mahbub';
+echo ' hey my name is ' . $name;
+echo "hey my name is $name"; // use "
+
+//strlen() - Return the Length of a String
+echo strlen("Hello world!"); // outputs 12
+
+//str_word_count() - Count Words in a String
+echo str_word_count("Hello world!"); // outputs 2
+
+//strrev() - Reverse a String
+echo strrev("Hello world!"); // outputs !dlrow olleH
+
+//strpos() - Search For a Text Within a String
+echo strpos("Hello world!", "world"); // outputs 6
+
+//str_replace() - Replace Text Within a String
+echo str_replace("world", "Dolly", "Hello world!"); // outputs Hello Dolly!
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>string</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
